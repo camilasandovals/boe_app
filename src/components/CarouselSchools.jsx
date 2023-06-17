ff73f3467258f68b84be6216a1be16b97294c79c
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-06-17 18:59:46 -0400

    cards created

---
+++ src/components/CarouselSchools.jsx
@@ -0,0 +1,49 @@
+export default function Carousel() {
+    return(
+    <>
+        <div class="slider">
+            <div class="slide-track">
+            <div class="slide">
+                <img src="../images/company-logos/ironhack.png" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../images/company-logos/atlantis-aviation.png" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../images/company-logos/paul-mitchell-mia.jpeg" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../images/company-logos/yatc.png" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../images/company-logos/plumbers-519.png" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="../images/company-logos/abc-institute.png" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/4G_LOGO_NEGRO-01__1_/original.png?1560209943" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="https://www.brighteducationinstitute.com/wp-content/uploads/2021/08/BEI.png" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="https://res-1.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/srbjs6l8gsg2lscg2p6j" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="https://arpec.org/wp-content/uploads/2021/11/cropped-ARPEC-logo-2021.png" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="https://floridatrainingservices.com/wp-content/themes/fltrnsvc/images/header-logo.png" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="https://wepowerfl.com/wp-content/uploads/2020/06/International-Brotherhood-of-Electrical-Workers-IBEW-Union-Contractors.png" height="100" width="100" alt="" />
+            </div>
+            <div class="slide">
+                <img src="https://southfloridatechnicaltraining.com/logo.svg" height="100" width="100" alt="" />
+            </div>
+            </div>
+        </div>
+    </>
+    )
+}
--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,29 +1,29 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import AppLayout from "../layout/AppLayout";
 import CarouselEvents from "../components/CarouselEvents";
+import CarouselSchools from "../components/CarouselSchools";
+import CardBlog from "../components/CardBlog";
 
 export default function Resources() {
     return(
         <AppLayout>
             <Container className="resources">
                 <h1>Resources</h1>
+                <CarouselSchools />
                 <Row>
                     <Col>
                         <h2>Upcoming events</h2>
                         <CarouselEvents />
                     </Col>
                     <Col>
-                        <h2>Visit our blog</h2>
-                        <p>carousel</p>
+                        <h2>Exclusive content</h2>
+                        <button className="button-class">Create account</button>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                        <h2>Visit our partner school</h2>
-                    </Col>
-                    <Col>
-                        <h2>Exclusive content</h2>
-                        <button className="button-class">Create account</button>
+                        <h2>Visit our blog</h2>
+                        <CardBlog />
                     </Col>
                 </Row>
             </Container>
