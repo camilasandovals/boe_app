13dd02af34c0612c0e64deac215be8d2677fee5a
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-06-23 01:05:26 -0400

    added footer

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -6,7 +6,7 @@ export default function AppLayout({children}) {
         <>
             <NavMenu />
             {children}
-            {/* <Footer /> */}
+            <Footer />
         </>
     )
 }
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,22 +4,23 @@ import { Facebook, Instagram, Linkedin } from "react-bootstrap-icons";
 export default function Footer() {
     return(
         <footer className="footer">
-        <div className="container">
-            <div className="row">
-            <div className="col">
+        <div className="footer-container">
+            <div >
                 <p><strong>Contact us</strong></p>
                 <p>305-903-2980</p>
                 <p>786-281-8944</p>
                 <p>info@boepartners.com</p>
             </div>
-            <div className="col">
-                <p><strong>Social media</strong></p>
-                <Instagram />
-                <Facebook />
-                <Linkedin />
+            <div >
+                {/* <p><strong>Social media</strong></p> */}
+                <Instagram size={30} style={{marginRight:20}}/> 
+                <Facebook size={30} style={{marginRight:20}}/> 
+                <Linkedin size={30} />
+            </div>
+            <div>
+                <h3>Subscribe to our newsletter</h3>
             </div>
             </div>
-        </div>
         </footer>
     )
 }
