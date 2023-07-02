3a460c4a009e3736554578901399ed7adb76ec4f
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-07-02 17:06:55 -0400

    added more things

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -3,8 +3,11 @@ import AppLayout from "../layout/AppLayout";
 import CarouselEvents from "../components/CarouselEvents";
 import CarouselSchools from "../components/CarouselSchools";
 import CardBlog from "../components/CardBlog";
+import { useNavigate } from "react-router-dom";
+import SignModal from "../components/SignModal";
 
 export default function Resources() {
+    const navigate = useNavigate()
     return(
         <AppLayout>
             <Container className="resources">
@@ -17,20 +20,21 @@ export default function Resources() {
                     </Col>
                     <Col>
                         <h2>Exclusive Content</h2>
-                        <div className="exclusive-container">
+                        <div className="exclusive-container" >
+                            <div className="exclusive-image-container" >
+                                <img src="/images/Guidecover.png" alt="A guide to careers in the skilled trades" />
+                            </div>
                             <div className="exclusive-text-container">
-                            <div>
-                                <h3>Benefits</h3>
-                                <p>Exclusive content</p>
-                                <p>Exclusive content</p>
-                                <p>Exclusive content</p>
+                                <h4>Sign in to unlock</h4>
+                                <div className='text-center'>
+                                <button className='button-class m-2 mb-3' onClick={() => navigate("/login")}>
+                                    Sign in
+                                </button>
+                                <button className='button-class m-2 mb-3' onClick={() => navigate("/signup")}>
+                                    Sign up
+                                </button>
+                                </div>
                             </div>
-                            <div>
-                                <p><strong>Create account to unlock</strong></p>
-                                <button className="button-class">Create Account</button>
-                            </div>
-                            </div>
-                            <img src="/images/Guidecover.png" alt="A guide to careers in the skilled trades" />
                         </div>
                     </Col>
                 </Row>
