d8497b23214d5de6870bbb4ebf018c69241d8fa4
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-07-08 16:20:55 -0400

    added favorites

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap";
 import AppLayout from "../layout/AppLayout";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../App";
 import Subscribe from "../components/Subscribe";
@@ -8,6 +8,7 @@ import { PencilFill } from "react-bootstrap-icons";
 
 export default function Profile() {
     const [user, setUser] = useContext(UserContext)
+    const [favorites, setFavorites] = useState([])
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -15,7 +16,13 @@ export default function Profile() {
             navigate('/signup');
             return;
         }
-    } , [])
+        fetch(`http://localhost:3000/userlikes?user=${user.email}`)
+            .then((response) => response.json())
+            .then(setFavorites)
+            .catch(console.error);
+        }, []);
+
+
     return(
         <AppLayout>
             <Container >
@@ -29,19 +36,33 @@ export default function Profile() {
                             <div className="image-container"> 
                                 {user?.image? <img src={user.image} alt="User profile" /> : <img src="/images/user-avatar.png" alt="Default user image" />}
                             </div>
-                            <div className="profile-info">
                                 <h2>{user?.firstName} {user?.lastName}</h2>
                                 <p>{user?.email}</p>
                                 <p>{user?.bio}</p>
                                 <p>{user?.city}, {user?.state}</p>
                                 <p>{user?.category}</p>
-                                {user?.skills?.map((skill, index) => (<span key={index} style={{paddingLeft:10, fontWeight:300}}>{skill}</span>))}
+                            <div className="skills">
+                                {user?.skills?.map((skill, index) => (<span key={index} style={{paddingLeft:10, fontWeight:200}}>{skill}</span>))}
                             </div>
                         </div>
                     </Col>
                     <Col sm={12} md={6}>
-                        <div className="favorites-container">
+                        <div className="profile-container">
                             <h3>Favorites</h3>
+                        <div className="favorites-container">
+                            {favorites? favorites.filter(favorite => favorite.is_liked).map((favorite, index) => (
+                                <div key={index} className="favorite">
+                                    {/* <div className="favorite-image">
+                                        <img src={favorite.image} alt="School logo" />
+                                    </div> */}
+                                    <div className="favorite-info">
+                                        <p><strong>{favorite.school}</strong></p>
+                                        <p>{favorite.program}</p>
+                                    </div>
+                                </div>
+                            )
+                            ) : <p>No favorites yet</p>}
+                        </div>
                         </div>
                     </Col>
                 </Row>
