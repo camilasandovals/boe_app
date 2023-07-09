e24c4371d365d8898ef59b249875969fa9f6d286
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-07-08 22:39:36 -0400

    improved

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -76,55 +76,63 @@ const handleCheckboxChange = (event) => {
 };
 
 
-    return (
-        <Form className="form-account" onSubmit={handleUpdateUser}>
-            <Form.Group className="m-2">
-                <div className="d-flex">
-                    <Form.Control type="text" value={firstName}  placeholder="Firstname"
-                    onChange={(e) => {setFirstName(e.target.value)}}/>
-                    <Form.Control type="text" value={lastName} placeholder="Lastname"
-                    onChange={(e) => {setLastName(e.target.value)}}/>
-                </div>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Control as="textarea" rows={3} placeholder="Tell us about yourself..."
-                onChange={(e) => {setBio(e.target.value)}}/>
-                </Form.Group>
-                <div className="d-flex">
-                <Form.Control type="text" value={city}  placeholder="City"
-                onChange={(e) => {setCity(e.target.value)}}/>
-                <Form.Select aria-label="Default select example" onChange={(e) => setState(e.target.value)}>
-                <option value="">Select state</option>
-                <option value="Florida">Florida</option>
-                </Form.Select>
-                </div>
-                Select the reason that best describes why you're creating a BOE account
-                <Form.Select aria-label="Default select example" onChange={(e) => setCategory(e.target.value)}>
-                <option value="">Select category</option>
-                <option value="I'm seeking a job">I'm seeking a job</option>
-                <option value="I'm just browsing">I'm just browsing</option>
-                <option value="I work at an organization that helps people with job placement or career search">I work at an organization that helps people with job placement or career search</option>
-                <option value="I know someone that might be interested">I know someone that might be interested</option>
-                </Form.Select>
-                Areas of interest
-                <Row>
-                    {array.map((type, index) => (
-                    <Col xs={4} key={index}>
-                        <Form.Check
-                          inline
-                          label={type}
-                          name="group1"
-                          type="checkbox"
-                          id={`inline-checkbox-${index}`}
-                          value={type}
-                          onChange={handleCheckboxChange}
-                        />
-                    </Col>
-                    ))}
-                </Row>
-            </Form.Group>
-            <button className="button-class" type="submit">
-                <strong>Submit</strong>
-            </button>
-        </Form>
-    )
+  return (
+    <Form className="form-account" onSubmit={handleUpdateUser}>
+        <Form.Group className="mb-3">
+            <div className="d-flex justify-content-between">
+                <Form.Control type="text" value={firstName}  placeholder="Firstname"
+                onChange={(e) => {setFirstName(e.target.value)}} className="me-2"/>
+                <Form.Control type="text" value={lastName} placeholder="Lastname"
+                onChange={(e) => {setLastName(e.target.value)}}/>
+            </div>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Control as="textarea" rows={3} placeholder="Tell us about yourself..."
+            onChange={(e) => {setBio(e.target.value)}}/>
+        </Form.Group>
+        <Form.Group className="mb-3">
+            <div className="d-flex justify-content-between">
+            <Form.Control type="text" value={city}  placeholder="City"
+            onChange={(e) => {setCity(e.target.value)}} className="me-2"/>
+            <Form.Select aria-label="Default select example" onChange={(e) => setState(e.target.value)}>
+            <option value="">Select state</option>
+            <option value="Florida">Florida</option>
+            </Form.Select>
+            </div>
+        </Form.Group>
+        <Form.Group className="mb-3">
+            Select the reason that best describes why you're creating a BOE account
+            <Form.Select aria-label="Default select example" onChange={(e) => setCategory(e.target.value)}>
+            <option value="">Select category</option>
+            <option value="I'm seeking a job">I'm seeking a job</option>
+            <option value="I'm just browsing">I'm just browsing</option>
+            <option value="I work at an organization that helps people with job placement or career search">I work at an organization that helps people with job placement or career search</option>
+            <option value="I know someone that might be interested">I know someone that might be interested</option>
+            </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3">
+            Areas of interest
+            <Row>
+                {array.map((type, index) => (
+                <Col xs={4} key={index}>
+                    <Form.Check
+                      inline
+                      label={type}
+                      name="group1"
+                      type="checkbox"
+                      id={`inline-checkbox-${index}`}
+                      value={type}
+                      onChange={handleCheckboxChange}
+                    />
+                </Col>
+                ))}
+            </Row>
+        </Form.Group>
+        <Form.Group className="mb-3">
+        <button className="button-class" type="submit">
+            <strong>Submit</strong>
+        </button>
+        </Form.Group>
+    </Form>
+  )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,8 +14,8 @@ export default function Login() {
             <Row className="login">
                 <h3>Kickstart a career in a skilled trade.</h3>
                 <h2>Login</h2>
-                <LoginForm endpoint= {endpoint} setUser ={setUser}/>
                 <p>Already have an account? <a href="/signup"><strong>Sign up</strong></a></p>
+                <LoginForm endpoint= {endpoint} setUser ={setUser}/>
             </Row>
         </AppLayout>
     )
